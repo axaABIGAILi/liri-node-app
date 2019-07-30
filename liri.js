f309712acad62080c8c762017fd3ce1d48d2230e
Abi D <axa.abigail.i@gmail.com>
2019-07-30 15:25:27 -0500

    populated code w/ more skeletal js, defined variables for artist and command input

--- liri.js
+++ liri.js
@@ -3,7 +3,12 @@ require("dotenv").config();
 // enables reading of keys file
 var keys = require("./keys.js");
 // spotify access
-var spotify = new Spotify(keys.spotify);
+//var spotify = new Spotify(keys.spotify);
+// bandsintown reqs
+var BandsInTownEvents = require('bandsintown-events');
+var Events = new BandsInTownEvents();
+// inquirer
+//var inquirer = require("inquirer");
 
 /* commands:
 - concert-this
@@ -14,19 +19,33 @@ var spotify = new Spotify(keys.spotify);
 
 // variable to store artist input
 var artist = process.argv[3];
+// variable to store command
+var command = process.argv[2];
+// use object to set Events parameters
+Events.setParams({
+    "app_id":"LIRI APP", //can be anything
+    "artists": artist
+  });
 
-// switch statement to evaluate input and output appropriate data
-switch (process.argv[2]) {
-    case 'concert-this': 
-        console.log();
-        break;
-    case 'spotify-this-song':
-        console.log();
-        break;
-    case 'movie-this':
-        console.log();
-        break;
-    case 'do-what-it-says':
-        console.log();
-        break;
-};
+// switch statement to evaluate the command and output appropriate
+switch (command) {
+  case 'concert-this':
+    // code
+    break;
+  case 'spotify-this-song':
+    // code
+    break;
+  case 'movie-this':
+    // code
+    break;
+  case 'do-what-it-says':
+    // code
+    break;
+}
+
+// start w/ first command 'concert this' 
+Events.getEvents(function(events) {
+    console.log(events);
+});
+
+// inquirer 
